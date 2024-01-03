4766d82ee16c60830a244d3b9b09e18f04264679
MUHAMMAD ALI <malirashid1994@gmail.com>
2024-01-03 21:07:11 +0500

    Solved 643

---
+++ Strings/643. Maximum Average Subarray I/maximum_average_subarray_I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        windowSum = sum(nums[:k])
+        windowAvg = windowSum/k
+        maxAvg = windowAvg
+
+        for i in range(len(nums)-k):
+            windowSum = windowSum - nums[i] + nums[i+k]
+            windowAvg = windowSum/k
+
+            maxAvg = max(maxAvg , windowAvg)
+
+        return
